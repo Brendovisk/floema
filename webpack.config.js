9010e926e12ebd6eea97e4bcdd1a32b52d9ad1ad
Brendon <brendon.franco@vibryt.com.br>
2023-02-18 22:12:11 -0300

    feat: add the first packages to the project

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
+
+const dirApp = path.join(__dirname, "app");
+const dirShared = path.join(__dirname, "shared");
+const dirStyles = path.join(__dirname, "styles");
+const dirNode = "node_modules";
+
+module.exports = {
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+
+  resolve: {
+    modules: [dirApp, dirShared, dirStyles, dirNode],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./shared",
+          to: "",
+        },
+      ],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|woff|woff2|webp)$/,
+        loader: "file-loader",
+        options: {
+          name(file) {
+            return "[hash].[ext]";
+          },
+        },
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|woff|woff2|webp)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              severityError: "warning",
+              minimizerOptions: {
+                plugins: [["gifsicle"]],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
