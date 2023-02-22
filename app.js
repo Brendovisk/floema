c3d0448229036d4e3f5825fa56873112d0011475
Brendon <brendon.franco@vibryt.com.br>
2023-02-22 00:38:42 -0300

    refactor: routes

--- app.js
+++ app.js
@@ -21,11 +21,38 @@ app.use(methodOverride())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+// Init prismic api
+const initApi = (req) => {
+  return prismic.createClient(process.env.PRISMIC_REPO_NAME, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+    fetch
+  })
+}
+
+const HandleLinkResolver = (doc) => {
+  if (doc.type === 'product') {
+    return `/detail/${doc.slug}`
+  }
+
+  if (doc.type === 'collections') {
+    return '/collections'
+  }
+
+  if (doc.type === 'about') {
+    return '/about'
+  }
+
+  // Default to homepage
+  return '/'
+}
+
 // Add a middleware function that runs on every route. It will inject
 // the prismic context to the locals so that we can access these in
 // our templates.
 app.use((req, res, next) => {
   res.locals.PrismicH = prismicH
+  res.locals.Link = HandleLinkResolver
 
   // Add a helper function to list all the collections
   res.locals.Numbers = (index) => {
@@ -43,55 +70,73 @@ app.use((req, res, next) => {
   next()
 })
 
-// Init prismic api
-const initApi = (req) => {
-  return prismic.createClient(process.env.PRISMIC_REPO_NAME, {
-    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-    req,
-    fetch
-  })
+const handleRequest = async (api) => {
+  const [meta] = await api.getAllByType('meta')
+  const [preloader] = await api.getAllByType('preloader')
+  const [navigation] = await api.getAllByType('navigation')
+
+  return {
+    meta,
+    navigation,
+    preloader
+  }
 }
 
 // Routes
 app.get('/', async (req, res) => {
   const api = await initApi(req)
-  const [meta] = await api.getAllByType('meta')
-  const [preloader] = await api.getAllByType('preloader')
+  const defaults = await handleRequest(api)
   const [home] = await api.getAllByType('home')
   const collections = await api.getAllByType('collection', {
     fetchLinks: 'product.image'
   })
-  res.render('pages/home', { meta, preloader, home, collections })
+
+  console.log({ ...defaults }.navigation.data.list)
+
+  res.render('pages/home', {
+    ...defaults,
+    home,
+    collections
+  })
 })
 
 app.get('/about', async (req, res) => {
   const api = await initApi(req)
-  const [meta] = await api.getAllByType('meta')
-  const [preloader] = await api.getAllByType('preloader')
+  const defaults = await handleRequest(api)
   const [about] = await api.getAllByType('about')
-  res.render('pages/about', { meta, preloader, about })
+
+  res.render('pages/about', {
+    ...defaults,
+    about
+  })
 })
 
 app.get('/detail/:uid', async (req, res) => {
   const api = await initApi(req)
-  const uid = req.params.uid
-  const [meta] = await api.getAllByType('meta')
-  const [preloader] = await api.getAllByType('preloader')
-  const product = await api.getByUID('product', uid, {
+  const defaults = await handleRequest(api)
+  const product = await api.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
   })
-  res.render('pages/detail', { meta, preloader, product })
+
+  res.render('pages/detail', {
+    ...defaults,
+    product
+  })
 })
 
 app.get('/collections', async (req, res) => {
   const api = await initApi(req)
-  const [meta] = await api.getAllByType('meta')
-  const [preloader] = await api.getAllByType('preloader')
+  const defaults = await handleRequest(api)
   const [home] = await api.getAllByType('home')
   const collections = await api.getAllByType('collection', {
     fetchLinks: 'product.image'
   })
-  res.render('pages/collections', { meta, preloader, home, collections })
+
+  res.render('pages/collections', {
+    ...defaults,
+    home,
+    collections
+  })
 })
 
 app.listen(port, () => {
